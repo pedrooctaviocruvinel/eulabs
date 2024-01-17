31d71be8bcce63733e26e77a2a9745129f0d8bbc
pedrooctaviocruvinel <pedrooctaviocruvinel@gmail.com>
2024-01-17 19:59:16 -0300

    ResultWrapper created

---
+++ src/util/result_wrapper.go
@@ -0,0 +1,22 @@
+package util
+
+type ResultWrapper[T any] struct {
+	Success bool     `json:"success"`
+	Errors  []string `json:"errors"`
+	Data    T        `json:"data"`
+}
+
+func NewResultWrapperFailed[T any](e []string) ResultWrapper[T] {
+	return ResultWrapper[T]{
+		Success: false,
+		Errors:  e,
+	}
+}
+
+func NewResultWrapperSucceded[T any](d T) ResultWrapper[T] {
+	return ResultWrapper[T]{
+		Success: true,
+		Errors:  nil,
+		Data:    d,
+	}
+}
